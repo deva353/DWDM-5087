38f4fa92b0061f7748fd5cc0a888a25fd4e9890e
deva <114378128+deva353@users.noreply.github.com>
2023-01-12 02:26:20 -0600

    Create 02_ScatterPlot.R

---
+++ EXERCISE 5/02_ScatterPlot.R
@@ -0,0 +1,14 @@
+# Data
+set.seed(9)
+x <- rnorm(1000)
+y <- rnorm(1000)
+
+# Smooth scatter plot
+smoothScatter(y ~ x)
+
+# Equivalent to:
+smoothScatter(x, y)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
